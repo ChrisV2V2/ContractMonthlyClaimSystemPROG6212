176d49fae44e5b5dc0b05aa1092c4bf12f55ce3e
Christo Mienie <128799213+ChrisV2V2@users.noreply.github.com>
2024-09-08 11:06:54 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ContractMonthlyClaimSystemPROG6212
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
